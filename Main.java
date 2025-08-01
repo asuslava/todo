b6fd0229b0df1c60057716c0811dc2f6013ea72c
bibop <165471701+asuslava@users.noreply.github.com>
2025-08-01 13:32:38 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,30 +1,102 @@
 package com.lisus;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class Main {
+    static final Scanner scanner = new Scanner(System.in);
+    static final List<Todo> todoList = new ArrayList<>();
+
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        List<Map<String, String>> todoList = new ArrayList<>();
-        LocalDate date = LocalDate.now();
+        while (true) {
 
-        // Format date
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd LLLL yyyy");
-        String formattedDate = date.format(formatter);
+            System.out.println("1 - new todo\n2 - all todos\n number - quit");
+            int action = scanner.nextInt();
+            scanner.nextLine();
 
-        System.out.println("Write the name of the task:");
+            switch (action) {
+                case 1: createTodo();
+                break;
+                case 2: showTodos();
+                break;
+                default:
+                    System.out.println("Return to menu");
+                    break;
+            }
+
+            System.out.println(".");
+        }
+
+    }
+
+    public static void createTodo() {
+        System.out.println("Write name of the task:");
         String todoName = scanner.nextLine();
 
-        System.out.println("Write status of your task:");
+        System.out.println("Write status of the task:");
         String todoStatus = scanner.nextLine();
 
-        Todo todo = new Todo(todoName, todoStatus, formattedDate);
-        todoList.add(todo.toMap());
+        Todo todo = new Todo(todoName, todoStatus, LocalDate.now());
+        todoList.add(todo);
+    }
+
+    public static void showTodos() {
+        System.out.println(todoList);
+        System.out.println("If you want to update - 'U', delete - 'D', quit - 'Q'");
+        String choice = scanner.nextLine();
+
+        switch (choice) {
+            case "U": updateTodo();
+                break;
+            case "D": removeTodo();
+                break;
+            case "Q":
+                System.out.println("Returning to menu");
+                break;
+            default:
+                System.out.println("Unknown option");
+                break;
+        }
+
         System.out.println(todoList);
 
+    }
+
+    public static void updateTodo() {
+
+        for (int i = 0; i < todoList.size(); i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("Write number of the task that you want to update:");
+        int todoNumber = scanner.nextInt();
+        scanner.nextLine();
+        Todo todo = todoList.get(todoNumber);
+        System.out.println(todo);
+
+        System.out.println("Write new name:");
+        String newTodoName = scanner.nextLine();
+        todo.name = newTodoName;
+
+        System.out.println("Write new status:");
+        String newTodoStatus = scanner.nextLine();
+        todo.status = newTodoStatus;
+        System.out.println(todo);
+
+    }
+
+    public static void removeTodo() {
+        for (int i = 0; i < todoList.size(); i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("Which todo you want to remove:");
+        int numberOfRemoveTodo = scanner.nextInt();
+        scanner.nextLine();
+
+        String removedTodo = String.valueOf(todoList.remove(numberOfRemoveTodo));
+        System.out.println("Deleted!");
     }
 
 }
--- Todo.java
+++ Todo.java
@@ -1,36 +1,24 @@
 package com.lisus;
 
-import java.util.Map;
-import java.util.HashMap;
+import java.time.LocalDate;
 import java.util.UUID;
 
 public class Todo {
-    String name;
+    static String name;
     String status;
-    String id;
-    String date;
+    UUID id;
+    static LocalDate date;
 
-    public Todo(String name, String status, String date) {
+    public Todo(String name, String status, LocalDate date) {
         this.name = name;
         this.status = status;
-        this.id = UUID.randomUUID().toString();
+        this.id = UUID.randomUUID();
         this.date = date;
     }
 
     @Override
     public String toString() {
-        return "name: " + this.name + "\nstatus: " + this.status + "\nid: " + this.id + "\ndate: " + this.date;
+        return "name: " + this.name + "\nstatus: " + this.status + "\nid: " + this.id + "\ndate: " + this.date.toString();
     }
-
-    public Map<String, String> toMap() {
-        Map<String, String> allTodos = new HashMap<>();
-        allTodos.put("Name", name);
-        allTodos.put("Status", status);
-        allTodos.put("ID", id);
-        allTodos.put("Date", date);
-        return allTodos;
-
-    }
-
 
 }
